1952b8249d5e88ba059f473d33d329f3b281af27
Alisa-Zhu <68450499+Alisa-Zhu@users.noreply.github.com>
2023-03-28 14:59:46 -0400

    Add files via upload

---
+++ frontend/src/app/services/user.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { BackendService } from '../services/backend.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+  name = '[USER\'S NAME]';
+  email = '[USER\'S EMAIL]';
+  favoriteColor = '[USER\'S FAVORITE COLOR]';
+
+  constructor(private backendService: BackendService) { }
+
+  ngOnInit() {
+    // Retrieve user's favorite color from the backend
+    this.backendService.getUserFavoriteColor().subscribe((color: string) => {
+      this.favoriteColor = color;
+      // Update background color to user's favorite color
+      document.body.style.backgroundColor = this.favoriteColor;
+    });
+  }}
